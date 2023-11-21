3ea3db6b845cce289f3a3b95d7a27619a1201896
antilles183 <antilles183@gmail.com>
2023-11-20 16:10:02 -0800

    player stub

--- src/Player.h
+++ src/Player.h
@@ -1,10 +1,17 @@
 #ifndef PLAYER_H
 #define PLAYER_H
+#include "States.h"
+#include "Event.h"
 
-class Player {
+class Player : public States
+{
     private:
 
     public:
+
+    // VIRTUAL OVERRIDES
+    void addEventHandler(Event event);
+    void update();
 };
 
 #endif // PLAYER_H
