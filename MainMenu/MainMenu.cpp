88f9618e2fbe74e45721590438b66a174cd1727b
StanleyZhang0 <115139825+StanleyZhang0@users.noreply.github.com>
2023-11-24 13:57:17 -0800

    decided to scrap the draft phase
    Since deadline is slowly approaching, we plan to only have 1 monster so drafting is not  necessary.

--- MainMenu/MainMenu.cpp
+++ MainMenu/MainMenu.cpp
@@ -31,64 +31,64 @@ void MainMenu::renderBlankNames() const
 
 /*----------DRAFT FUNCTIONS AND VARIABLES----------*/
 
-void MainMenu::draftMenu()  //function 1
-{
+// void MainMenu::draftMenu()  //function 1
+// {
 
-    //before:
-    //      |(0,1)|
-    //      |(1,1)|
-    //      |(2,1)|
-    //      |(3,1)|
-    //      |(4,1)|
-    //      |(5,1)|
-    std::string space [6][3] = {{"0", "1", "0"}, {"0", "2", "0"}, {"0", "3", "0"}, {"0", "4", "0"}, {"0", "5", "0"}, {"0", "6", "0"}};
-    //after:
-    // (1,0)|     |(1,2)
-    // (3,0)|     |(3,2)
-    // (5,0)|     |(5,2)
-    //std::string space [6][3] = {{"0", "0", "0"}, {"1", "0", "2"}, {"0", "0", "0"}, {"3", "0", "4"}, {"0", "0", "0"}, {"5", "0", "6"}};
+//     //before:
+//     //      |(0,1)|
+//     //      |(1,1)|
+//     //      |(2,1)|
+//     //      |(3,1)|
+//     //      |(4,1)|
+//     //      |(5,1)|
+//     std::string space [6][3] = {{"0", "1", "0"}, {"0", "2", "0"}, {"0", "3", "0"}, {"0", "4", "0"}, {"0", "5", "0"}, {"0", "6", "0"}};
+//     //after:
+//     // (1,0)|     |(1,2)
+//     // (3,0)|     |(3,2)
+//     // (5,0)|     |(5,2)
+//     //std::string space [6][3] = {{"0", "0", "0"}, {"1", "0", "2"}, {"0", "0", "0"}, {"3", "0", "4"}, {"0", "0", "0"}, {"5", "0", "6"}};
 
-    //this is placeholder
-    std::string n1; // <<player1
-    std::string n2; // <<player2
-    std::cout << "Enter the name of the first player: \n";
-    std::cin >> n1;
-    std::cout << "Enter the name of the second player: \n";
-    std::cin >> n2;
-    std::cout << n1 << " is player1 so he/she will choose first \n";
-    std::cout << n2 << " is player2 so he/she will choose second \n\n";
+//     //this is placeholder
+//     std::string n1; // <<player1
+//     std::string n2; // <<player2
+//     std::cout << "Enter the name of the first player: \n";
+//     std::cin >> n1;
+//     std::cout << "Enter the name of the second player: \n";
+//     std::cin >> n2;
+//     std::cout << n1 << " is player1 so he/she will choose first \n";
+//     std::cout << n2 << " is player2 so he/she will choose second \n\n";
 
-    std::cout << "==|===========================================|== \n";
-    std::cout << "  |                                           |   \n";
-    std::cout << "  | PLAYER1                           PLAYER2 |   \n";
-    std::cout << "  |-------------------------------------------|   \n";
-    std::cout << "  |                                           |   \n";
-    std::cout << "  |        " << space[0][0] << "        |   " << space[0][1] << "   |        " << space[0][2] << "        |  \n";
-    std::cout << "  |                                           |   \n";
-    std::cout << "  |        " << space[1][0] << "        |   " << space[1][1] << "   |        " << space[1][2] << "        |  \n";
-    std::cout << "  |                                           |   \n";
-    std::cout << "  |        " << space[2][0] << "        |   " << space[2][1] << "   |        " << space[2][2] << "        |  \n";
-    std::cout << "  |                                           |   \n";
-    std::cout << "  |        " << space[3][0] << "        |   " << space[3][1] << "   |        " << space[3][2] << "        |  \n";
-    std::cout << "  |                                           |   \n";
-    std::cout << "  |        " << space[4][0] << "        |   " << space[4][1] << "   |        " << space[4][2] << "        |  \n";
-    std::cout << "  |                                           |   \n";
-    std::cout << "  |        " << space[5][0] << "        |   " << space[5][1] << "   |        " << space[5][2] << "        |  \n";
-    std::cout << "  |                                           |   \n";
-    std::cout << "==|===========================================|== \n";
-    std::cout << "\n";
-    std::cout << "|=|===================|=|\n";
-    std::cout << "|=|                   |=|\n";
-    std::cout << "|=|  1.Monster1       |=|\n";
-    std::cout << "|=|  2.Monster2       |=|\n";
-    std::cout << "|=|  3.Monster3       |=|\n";
-    std::cout << "|=|  4.Monster4       |=|\n";
-    std::cout << "|=|  5.Monster5       |=|\n";
-    std::cout << "|=|  6.Monster6       |=|\n";
-    std::cout << "|=|                   |=|\n";
-    std::cout << "|=|===================|=|\n";
-    std::cout << "\n";
-}
+//     std::cout << "==|===========================================|== \n";
+//     std::cout << "  |                                           |   \n";
+//     std::cout << "  | PLAYER1                           PLAYER2 |   \n";
+//     std::cout << "  |-------------------------------------------|   \n";
+//     std::cout << "  |                                           |   \n";
+//     std::cout << "  |        " << space[0][0] << "        |   " << space[0][1] << "   |        " << space[0][2] << "        |  \n";
+//     std::cout << "  |                                           |   \n";
+//     std::cout << "  |        " << space[1][0] << "        |   " << space[1][1] << "   |        " << space[1][2] << "        |  \n";
+//     std::cout << "  |                                           |   \n";
+//     std::cout << "  |        " << space[2][0] << "        |   " << space[2][1] << "   |        " << space[2][2] << "        |  \n";
+//     std::cout << "  |                                           |   \n";
+//     std::cout << "  |        " << space[3][0] << "        |   " << space[3][1] << "   |        " << space[3][2] << "        |  \n";
+//     std::cout << "  |                                           |   \n";
+//     std::cout << "  |        " << space[4][0] << "        |   " << space[4][1] << "   |        " << space[4][2] << "        |  \n";
+//     std::cout << "  |                                           |   \n";
+//     std::cout << "  |        " << space[5][0] << "        |   " << space[5][1] << "   |        " << space[5][2] << "        |  \n";
+//     std::cout << "  |                                           |   \n";
+//     std::cout << "==|===========================================|== \n";
+//     std::cout << "\n";
+//     std::cout << "|=|===================|=|\n";
+//     std::cout << "|=|                   |=|\n";
+//     std::cout << "|=|  1.Monster1       |=|\n";
+//     std::cout << "|=|  2.Monster2       |=|\n";
+//     std::cout << "|=|  3.Monster3       |=|\n";
+//     std::cout << "|=|  4.Monster4       |=|\n";
+//     std::cout << "|=|  5.Monster5       |=|\n";
+//     std::cout << "|=|  6.Monster6       |=|\n";
+//     std::cout << "|=|                   |=|\n";
+//     std::cout << "|=|===================|=|\n";
+//     std::cout << "\n";
+// }
 
 // void MainMenu::activeDraftMenu()    //function 2
 // {}
