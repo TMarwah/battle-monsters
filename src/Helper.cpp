b76f6ee567be133f27d876c33a54ab7f4dc9efbf
antilles183 <antilles183@gmail.com>
2023-11-24 18:45:43 -0800

    making logger for GameState

--- src/Helper.cpp
+++ src/Helper.cpp
@@ -1,25 +1,44 @@
 #include "Helper.h"
 
-void Helper::logState(States statesObject, 
-                      std::string objectName)
+void Helper::logState(std::string objectName, 
+                      GameState state)
 {
-    std::cout << "Helper::logStates\n";
+    std::cout << "Helper::logState\n";
     std::cout << "\tobject: " << objectName << "\n";
-    // std::cout << "\t\tstate: ";
-    if(statesObject.checkState(ACTIVE)) {std::cout << "\t\tACTIVE\n";}
-    if(statesObject.checkState(RUNNING)) {std::cout << "\t\tRUNNING\n";}
-    if(statesObject.checkState(START_UP)) {std::cout << "\t\tSTART_UP\n";}
-    if(statesObject.checkState(INIT)) {std::cout << "\t\tINIT\n";}
-    if(statesObject.checkState(DRAFT)) {std::cout << "\t\tDRAFT\n";}
-    if(statesObject.checkState(ATTACKING)) {std::cout << "\t\tATTACKING\n";}
-    if(statesObject.checkState(DEFENDING)) {std::cout << "\t\tDEFENDING\n";}
-    if(statesObject.checkState(BATTLE)) {std::cout << "\t\tBATTLE\n";}
-    if(statesObject.checkState(BATTLE_OVER)) {std::cout << "\t\tBATTLE_OVER\n";}
-    if(statesObject.checkState(REPLAY)) {std::cout << "\t\tREPLAY\n";}
+    // if(state == ACTIVE_ST) {std::cout << "\t\tACTIVE_ST\n";}
+    // if(state == RUNNING_ST) {std::cout << "\t\tRUNNING_ST\n";}
+    if(state == START_UP_ST) {std::cout << "\t\tSTART_UP_ST\n";}
+    if(state == PLAYERS_SETUP_ST) {std::cout << "\t\tPLAYERS_SETUP_ST\n";}
+    if(state == DRAFT_ST) {std::cout << "\t\tDRAFT_ST\n";}
+    // if(state == ATTACKING_ST) {std::cout << "\t\tATTACKING_ST\n";}
+    // if(state == DEFENDING_ST) {std::cout << "\t\tDEFENDING_ST\n";}
+    if(state == BATTLE_ST) {std::cout << "\t\tBATTLE_ST\n";}
+    if(state == BATTLE_OVER_ST) {std::cout << "\t\tBATTLE_OVER_ST\n";}
+    if(state == PLAY_AGAIN_ST) {std::cout << "\t\tPLAY_AGAIN_ST\n";}
+    if(state == QUIT_ST) {std::cout << "\t\tQUIT_ST\n";}
     std::cout << '\n';
 
 }
 
+// void Helper::logState(States statesObject, 
+//                       std::string objectName)
+// {
+//     std::cout << "Helper::logStates\n";
+//     std::cout << "\tobject: " << objectName << "\n";
+//     // std::cout << "\t\tstate: ";
+//     if(statesObject.checkState(ACTIVE)) {std::cout << "\t\tACTIVE\n";}
+//     if(statesObject.checkState(RUNNING)) {std::cout << "\t\tRUNNING\n";}
+//     if(statesObject.checkState(START_UP)) {std::cout << "\t\tSTART_UP\n";}
+//     if(statesObject.checkState(PLAYERS_SETUP)) {std::cout << "\t\tPLAYERS_SETUP\n";}
+//     if(statesObject.checkState(DRAFT)) {std::cout << "\t\tDRAFT\n";}
+//     if(statesObject.checkState(ATTACKING)) {std::cout << "\t\tATTACKING\n";}
+//     if(statesObject.checkState(DEFENDING)) {std::cout << "\t\tDEFENDING\n";}
+//     if(statesObject.checkState(BATTLE)) {std::cout << "\t\tBATTLE\n";}
+//     if(statesObject.checkState(BATTLE_OVER)) {std::cout << "\t\tBATTLE_OVER\n";}
+//     if(statesObject.checkState(PLAY_AGAIN)) {std::cout << "\t\tPLAY_AGAIN\n";}
+//     std::cout << '\n';
+
+// }
 
 void Helper::logEvent(Event event, 
                       std::string callLocation)
@@ -27,7 +46,7 @@ void Helper::logEvent(Event event,
     std::cout << "Helper::logEvent\n";
     std::cout << "\tcall location: " << callLocation << "\n";
     std::cout << "\t\teventType: " << event.eventType << "\n";
-    std::cout << "\t\tdata1  : " << event.data << "\n";
+    std::cout << "\t\tdata1  : " << event.data1 << "\n";
     std::cout << "\t\tdata2  : " << event.data2 << "\n";
     std::cout << '\n';
 
--- src/Helper.h
+++ src/Helper.h
@@ -1,14 +1,13 @@
 #pragma once
 #include <iostream>
-#include "enum/ObjectStates.h"
-#include "States.h"
+#include "enum/GameState.h"
 #include "Event.h"
 
 
 class Helper {
 public:
-    static void logState(States statesObject,
-                         std::string objectName);
+    static void logState(std::string objectName,
+                         GameState state);
 
     static void logEvent(Event event,
                          std::string callLocation);
