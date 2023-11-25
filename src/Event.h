19e94ea94b3e6bcd6aaf41e2ec2c3012f8544435
antilles183 <antilles183@gmail.com>
2023-11-24 18:49:39 -0800

    developing main game loop

--- src/Event.h
+++ src/Event.h
@@ -1,10 +1,11 @@
 #pragma once
+#include <string>
 #include "enum/EventType.h"
 
 struct Event
 {
     EventType eventType {EventType::NULL_EVENT};
-    std::string data = "null";
+    std::string data1 = "null";
     std::string data2 = "null";
 
 };
