59b0059733c75f02c1f8d934f6306a94a3d05b22
antilles183 <antilles183@gmail.com>
2023-11-24 18:58:26 -0800

    renamed ObjectStates enum to more descriptive GameState

---
+++ src/enum/GameState.h
@@ -0,0 +1,21 @@
+#pragma once
+
+enum GameState
+{
+    // ACTIVE_ST,
+    // RUNNING_ST,
+    START_UP_ST,
+    PLAYERS_SETUP_ST,
+    DRAFT_ST,
+    // ATTACKING_ST,
+    // DEFENDING_ST,
+    BATTLE_ST,
+    BATTLE_OVER_ST,
+    PLAY_AGAIN_ST,
+    QUIT_ST,
+    NULL_ST,
+    // add any new state you want above this line. as long as LAST_STATE is the
+    // last state in the enum our loops will not need updating
+    LAST_ST
+
+};
