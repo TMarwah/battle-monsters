a7ce4458d03ce7bdab8ef1805284d8a466e44da3
StanleyZhang0 <115139825+StanleyZhang0@users.noreply.github.com>
2023-11-25 23:45:53 -0800

    commented out 2nd healthbar

--- src/HealthBar.cpp
+++ src/HealthBar.cpp
@@ -12,30 +12,30 @@ void HealthBars::setMonster1HP(int firstMonsterHP)
     this->_health1 = firstMonsterHP;
 }
 
-void HealthBars::setMonster2HP(int secondMonsterHP)
-{
-    this->_health2 = secondMonsterHP;
-}
+// void HealthBars::setMonster2HP(int secondMonsterHP)
+// {
+//     this->_health2 = secondMonsterHP;
+// }
 
 int HealthBars::getMonster1HP()
 {
     return this->_health1;
 }
 
-int HealthBars::getMonster2HP()
-{
-    return this->_health2;
-}
+// int HealthBars::getMonster2HP()
+// {
+//     return this->_health2;
+// }
 
 void HealthBars::deductHealthMonster1(int incomingDamage)
 {
     this->_health1 = this->_health1 - incomingDamage;
 }
 
-void HealthBars::deductHealthMonster2(int incomingDamage)
-{
-    this->_health2 = this->_health2 - incomingDamage;
-}
+// void HealthBars::deductHealthMonster2(int incomingDamage)
+// {
+//     this->_health2 = this->_health2 - incomingDamage;
+// }
 
 bool HealthBars::checkIfZero()
 {
