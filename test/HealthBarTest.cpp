91f50e2047f46970e4bc3659234b4291ffa3cc21
StanleyZhang0 <115139825+StanleyZhang0@users.noreply.github.com>
2023-12-01 19:51:09 -0800

    updated naming convention for HealthBar tests

--- test/HealthBarTest.cpp
+++ test/HealthBarTest.cpp
@@ -2,20 +2,19 @@
 #include "../src/HealthBar.h"
 #include <iostream>
 
-TEST(HealthBarsTest, DefaultConstructorTest)
+TEST(HealthBarTest, DefaultConstructorTest)
 {
     //Arrange / Act
-    HealthBars _healthbar;
+    HealthBar _healthbar;
 
     //Assert
     EXPECT_EQ(_healthbar.getMonster1HP(), 0);
-    // EXPECT_EQ(_healthbar.getMonster2HP(), 0);
 }
 
-TEST(HealthBarsTest, Monster1HealthSetter_1)
+TEST(HealthBarTest, Monster1HealthSetter_1)
 {
     //Arrange 
-    HealthBars _healthbar;
+    HealthBar _healthbar;
 
     //Act
     _healthbar.setMonster1HP(50);
@@ -24,10 +23,10 @@ TEST(HealthBarsTest, Monster1HealthSetter_1)
     EXPECT_EQ(_healthbar.getMonster1HP(), 50);
 }
 
-TEST(HealthBarsTest, Monster1HealthSetter_2)
+TEST(HealthBarTest, Monster1HealthSetter_2)
 {
     //Arrange
-    HealthBars _healthbar;
+    HealthBar _healthbar;
 
     //Act
     _healthbar.setMonster1HP(150);
@@ -36,34 +35,10 @@ TEST(HealthBarsTest, Monster1HealthSetter_2)
     EXPECT_EQ(_healthbar.getMonster1HP(), 150);
 }
 
-// TEST(HealthBarsTest, Monster2HealthSetter_1)
-// {
-//     //Arrange
-//     HealthBars _healthbar;
-
-//     //Act
-//     _healthbar.setMonster2HP(50);
-
-//     //Assert
-//     EXPECT_EQ(_healthbar.getMonster2HP(), 50);
-// }
-
-// TEST(HealthBarsTest, Monster2HealthSetter_2)
-// {
-//     //Arrange
-//     HealthBars _healthbar;
-
-//     //Act
-//     _healthbar.setMonster2HP(150);
-
-//     //Assert
-//     EXPECT_EQ(_healthbar.getMonster2HP(), 150);    
-// }
-
-TEST(HealthBarsTest, ReduceMonster1Health_1)
+TEST(HealthBarTest, ReduceMonster1Health_1)
 {
     //Arrange
-    HealthBars _healthbar;
+    HealthBar _healthbar;
 
     //Act
     _healthbar.setMonster1HP(50);
@@ -73,10 +48,10 @@ TEST(HealthBarsTest, ReduceMonster1Health_1)
     EXPECT_EQ(_healthbar.getMonster1HP(), 25);
 }
 
-TEST(HealthBarsTest, ReduceMonster1Health_2)
+TEST(HealthBarTest, ReduceMonster1Health_2)
 {
     //Arrange
-    HealthBars _healthbar;
+    HealthBar _healthbar;
 
     //Act
     _healthbar.setMonster1HP(150);
@@ -86,36 +61,10 @@ TEST(HealthBarsTest, ReduceMonster1Health_2)
     EXPECT_EQ(_healthbar.getMonster1HP(), 91);
 }
 
-// TEST(HealthBarsTest, ReduceMonster2Health_1)
-// {
-//     //Arrange
-//     HealthBars _healthbar;
-
-//     //Act
-//     _healthbar.setMonster2HP(50);
-//     _healthbar.deductHealthMonster2(25);
-
-//     //Assert
-//     EXPECT_EQ(_healthbar.getMonster2HP(), 25);
-// }
-
-// TEST(HealthBarsTest, ReduceMonster2Health_2)
-// {
-//     //Arrange
-//     HealthBars _healthbar;
-
-//     //Act
-//     _healthbar.setMonster2HP(150);
-//     _healthbar.deductHealthMonster2(59);
-
-//     //Assert
-//     EXPECT_EQ(_healthbar.getMonster2HP(), 91);
-// }
-
-TEST(HealthBarsTest, CheckForZeroHP_monster1)
+TEST(HealthBarTest, CheckForZeroHP_monster1)
 {
     //Arrange
-    HealthBars _healthbar;
+    HealthBar _healthbar;
 
     //Act
     _healthbar.setMonster1HP(150);
@@ -126,10 +75,10 @@ TEST(HealthBarsTest, CheckForZeroHP_monster1)
     EXPECT_TRUE(_healthbar.checkIfZero());
 }
 
-TEST(HealthBarsTest, CheckForZeroHP_monster1_1) //suppose to fail
+TEST(HealthBarTest, CheckForZeroHP_monster1_1) //suppose to fail
 {
     //Arrange
-    HealthBars _healthbar;
+    HealthBar _healthbar;
 
     //Act
     _healthbar.setMonster1HP(150);
@@ -139,31 +88,3 @@ TEST(HealthBarsTest, CheckForZeroHP_monster1_1) //suppose to fail
     EXPECT_EQ(_healthbar.getMonster1HP(), 0);
     EXPECT_TRUE(_healthbar.checkIfZero());
 }
-
-// TEST(HealthBarsTest, CheckForZeroHP_monster2)
-// {
-//     //Arrange
-//     HealthBars _healthbar;
-
-//     //Act
-//     _healthbar.setMonster2HP(150);
-//     _healthbar.deductHealthMonster2(150);
-
-//     //Assert
-//     EXPECT_EQ(_healthbar.getMonster2HP(), 0);
-//     EXPECT_TRUE(_healthbar.checkIfZero());
-// }
-
-// TEST(HealthBarsTest, CheckForZeroHP_monster2_1) //suppose to fail
-// {
-//     //Arrange
-//     HealthBars _healthbar;
-
-//     //Act
-//     _healthbar.setMonster2HP(150);
-//     _healthbar.deductHealthMonster2(149);
-
-//     //Assert
-//     EXPECT_EQ(_healthbar.getMonster2HP(), 0);
-//     EXPECT_TRUE(_healthbar.checkIfZero());
-// }
