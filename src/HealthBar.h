4167959b10225080f96fd761c9e32714e1f1c4d6
StanleyZhang0 <115139825+StanleyZhang0@users.noreply.github.com>
2023-12-01 19:52:06 -0800

    updated naming convention for HealthBar header file

--- src/HealthBar.h
+++ src/HealthBar.h
@@ -1,26 +1,22 @@
 #pragma once
 
-class HealthBars
+class HealthBar
 {
     private:
         int _health1;   //base health of monster1
-        // int _health2;   //base health of monster2
 
     public:
         //Constructor
-        HealthBars();
+        HealthBar();
 
         //Setters for monster's HP
         void setMonster1HP(int firstMonsterHP);
-        // void setMonster2HP(int secondMonsterHP);
 
         //Getters for monster's HP
         int getMonster1HP();
-        // int getMonster2HP();
 
         //
         void deductHealthMonster1(int incomingDamage);
-        // void deductHealthMonster2(int incomingDamage);
         bool checkIfZero();
 
 };
