eef92618bd1191e434530752f704b696c74ed356
antilles183 <antilles183@gmail.com>
2023-12-05 22:56:40 -0800

    implemented fight. updated event handler

--- src/Players.cpp
+++ src/Players.cpp
@@ -23,17 +23,115 @@ void Players::setNames(const Event& event) {
 
 }
 
-void Players::reset() {
-    _players[0].reset();
-    _players[1].reset();
+void Players::fight(Monster& monster1, Monster& monster2) {
+
+    std::cout << "Players::fight" << "\n";
+    
+    // TODO: implement priority feature
+    // TODO: decide or randomize which player goes first 
+    monster1.defend( monster2.attack() );
+    monster2.defend( monster1.attack() );
+
+    // test if battle is over
+    update();
+
 }
 
 /*******************************************************************************
 * ACCESSORS
 *******************************************************************************/
-Player& Players::getPlayer(unsigned index) const {
+// Player& Players::getPlayer(unsigned index) const {
+//     assert(index < NUM_OF_PLAYERS);
+
+//     return _players[index];
+
+// }
+
+/*******************************************************************************
+* VIRTUAL OVERRIDES
+*******************************************************************************/
+void Players::addEventHandler(const Event& event) {
+
+    switch(getState())
+    {
+        case DRAFT_ST:
+            handleDraft(event);
+            break;
+
+        case BATTLE_ST:
+            handleBattle(event);
+            break;
+
+    }
+
+}
+
+
+/*******************************************************************************
+* PRIVATE HELPERS
+*******************************************************************************/
+Player& Players::getPlayer(unsigned index) {
     assert(index < NUM_OF_PLAYERS);
 
     return _players[index];
+
+}
+
+// const Player& Players::getPlayer(unsigned index) const {
+//     assert(index < NUM_OF_PLAYERS);
+
+//     return _players[index];
+
+// }
+
+// Player& Players::getPlayer(unsigned index) const {
+//     assert(index < NUM_OF_PLAYERS);
+
+//     return _players[index];
+
+// }
+
+void Players::reset() {
+    _players[0].reset();
+    _players[1].reset();
+
+}
+
+
+void Players::update() {
+
+    // update players
+    getPlayer(0).update();
+    getPlayer(1).update();
+
+    // if either player is defeated then BATTLE_ST complete
+    if(getPlayer(0).isDefeated() || getPlayer(1).isDefeated()) {
+        setState(NULL_ST);
+    }
+
+}
+
+
+/*******************************************************************************
+* PRIVATE HELPERS - HANDLERS
+*******************************************************************************/
+void Players::handleDraft(const Event& event) {
+
+}
+
+void Players::handleBattle(const Event& event) {
+
+    std::cout << "Players::handleBattle" << "\n";
+
+    _players[0].setNextMove(event.data1);
+    _players[1].setNextMove(event.data2);
+
+    fight(_players[0].getBench()->getCurrent(),
+          _players[1].getBench()->getCurrent());
+
+    // if(event.eventType == EventType::ATTACK) {
+    //     _players[0].getBench()->getCurrent().getSkillset().setMoveIndex(event.data1);
+    //     _players[1].getBench()->getCurrent().getSkillset().setMoveIndex(event.data2);
+    // }
 
 }
--- src/Players.h
+++ src/Players.h
@@ -1,10 +1,12 @@
 #pragma once
 #include "State.h"
+#include "abstract/EventHandler.h"
 #include "Player.h"
 #include "definitions/Definitions.h"
+#include "enum/EventType.h"
 
 
-class Players : public State
+class Players : public State, public EventHandler
 {
     Player* _players;
 
@@ -15,9 +17,17 @@ public:
 
     // MUTATORS
     void setNames(const Event& event);
+    void fight(Monster& monster1, Monster& monster2);
     void reset();
+    void update();
 
     // ACCESSORS
-    Player& getPlayer(unsigned index) const;
+    Player& getPlayer(unsigned index);
+    // Player& getPlayer(unsigned index) const;
+    // const Player& getPlayer(unsigned index) const;
+
+    void addEventHandler(const Event& event);
+    void handleDraft(const Event& event);
+    void handleBattle(const Event& event);
 
 };
