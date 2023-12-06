0b18a608f2794e6d92d739ef5a4597cb68343812
williamEKim <dongyeon0308@gmail.com>
2023-12-05 19:35:45 -0800

    updated Draftboard to initialize Monster with movesets and stats

--- src/DraftBoard.cpp
+++ src/DraftBoard.cpp
@@ -19,35 +19,20 @@ DraftBoard::DraftBoard() {
 
     _draftBoard = new Monster[BENCH_SIZE * 2];
 
-    _draftBoard[0].setName("Andrewsaurus");
-    _draftBoard[1].setName("Willzilla");
-
-    // Monster* Andrewsaurus = new Monster();
-    // Monster* Willzilla = new Monster("Willzilla");
-
-    // Andrewsaurus->setName("Andrewsaurus");
-
-    // _draftBoard.push_back(Andrewsaurus);
-    // _draftBoard.push_back(Willzilla);
-
-
+    _draftBoard[0] = Monster("Andrewsaurus", NORMAL, 50, 70);
+    _draftBoard[1] = Monster("Willzilla", NORMAL, 60, 60);
 
     // 120 as a balance factor of stats = attack + defense
-    /*
-    Monster* Andrewsaurus = new Monster("Andrewsaurus", NORMAL, 50, 70);
+    // Monster* Andrewsaurus = new Monster("Andrewsaurus");
     std::vector<MoveSet *> AndrewsaurusMoveset;
     // MoveSets("name", accuracy, base_damage, priority(0 as default));
     AndrewsaurusMoveset.push_back(new NormalAttack("Rock-bottom", 0.9, 25, 1));
     AndrewsaurusMoveset.push_back(new NormalAttack("Charge", 0.4, 50, 0));
-    Andrewsaurus->addMoves(AndrewsaurusMoveset);
-    _draftBoard.push_back(Andrewsaurus);
+    _draftBoard[0].addMoves(AndrewsaurusMoveset);
 
-    Monster* Willzilla = new Monster("Willzilla", NORMAL, 60, 60);
     std::vector<MoveSet *> WillzillaMoveSet;
     WillzillaMoveSet.push_back(new NormalAttack("Lazer Breath", 0.7, 35, 0));
-    Willzilla->addMoves(WillzillaMoveSet);
-    _draftBoard.push_back(Willzilla);
-    */
+    _draftBoard[1].addMoves(WillzillaMoveSet);
     
 
 }
