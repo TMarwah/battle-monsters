a5b9ba455f4fbeea5888d88e8497da30416cee50
williamEKim <dongyeon0308@gmail.com>
2023-12-06 02:55:57 -0800

    now State.h includes Definitions.h

--- src/definitions/Definitions.h
+++ src/definitions/Definitions.h
@@ -3,9 +3,10 @@
 #define NUM_OF_PLAYERS 2
 #define BENCH_SIZE 1
 #define DISPLAY_UPTIME 1.0f     // type: float (in seconds)
-#define CLEAR_SCREEN false       // toggle system("clear") command
+#define ENDING_UPTIME 5.0f     // type: float (in seconds)
+#define CLEAR_SCREEN true       // toggle system("clear") command
 
 #define MONSTER_IMPLEMENTED true
-#define FLAG_ON true
+#define FLAG_ON false
 
 
