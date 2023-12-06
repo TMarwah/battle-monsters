0bb9f0d988077dc94913c5921e5088f25b9c2406
antilles183 <antilles183@gmail.com>
2023-12-05 17:10:56 -0800

    changed BENCH_SIZE to 1

--- src/definitions/Definitions.h
+++ src/definitions/Definitions.h
@@ -1,7 +1,7 @@
 #pragma once
 
 #define NUM_OF_PLAYERS 2
-#define BENCH_SIZE 3
+#define BENCH_SIZE 1
 #define DISPLAY_UPTIME 3.0f     // type: float (in seconds)
 #define CLEAR_SCREEN true       // toggle system("clear") command
 
