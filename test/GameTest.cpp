a53f1e770f7450fad400ade947bf9c7c662facc5
antilles183 <antilles183@gmail.com>
2023-12-07 22:12:33 -0800

    implemented unit test

--- test/GameTest.cpp
+++ test/GameTest.cpp
@@ -2,21 +2,26 @@
 #include "../src/enum/GameState.h"
 #include "../src/Game.h"
 
-// Test if constructor generates _players member variable
-// TEST(GameClassTests, testConstructor) {
-//     // call constructor
-//     Game game;
-//     // if nullptr, it will return false; therefore terminate
-//     ASSERT_TRUE(game.getPlayers()); 
-// }
+TEST(GameClassTests, StartUp){
 
-// Test init() method using checkState() method
-TEST(GameClassTests, testInitMethod){
     //Arrange
     Game game;
+    
     //ACT
     game.init();
+    
+    //Assert
+    EXPECT_TRUE(game.getState() == GameState::START_UP_ST);
+}
+
+TEST(GameClassTests, IsRunning){
+
+    //Arrange
+    Game game;
+    
+    //ACT
+    game.init();
+    
     //Assert
     EXPECT_TRUE(game.isRunning());
-    EXPECT_TRUE(game.getState() == GameState::START_UP_ST);
 }
